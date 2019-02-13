2676c0be04358bb7d6e3efbc3b99c902521842eb
jumbofile <gplachno@ycp.edu>
2019-02-13 16:50:00 -0500

    WASD Movement

--- Client/Assets/Scripts/Client.cs
+++ Client/Assets/Scripts/Client.cs
@@ -91,21 +91,23 @@ public class Client : MonoBehaviour
                 SceneManager.LoadScene("ClientGameView");
                 break;
             case "UnitSpawned":
-                GameObject prefab = Resources.Load("Prefabs/Unit1") as GameObject;
+                GameObject prefab = Resources.Load("Prefabs/OtherPlayer") as GameObject;
                 GameObject go = Instantiate(prefab);
                 float parsedX = float.Parse(aData[3], culture);
                 float parsedY = float.Parse(aData[4], culture);
                 float parsedZ = float.Parse(aData[5], culture);
-                go.GetComponent<NavMeshAgent>().Warp(new Vector3(parsedX, parsedY, parsedZ));
+                //go.GetComponent<NavMeshAgent>().Warp(new Vector3(parsedX, parsedY, parsedZ));
                 Unit un = go.AddComponent<Unit>();
                 unitsOnMap.Add(un);
                 int parsed;
                 Int32.TryParse(aData[2], out parsed);
                 un.unitID = parsed;
 
+                //if the unit spawned is owned by the player pass in the bool and activate the camera
                 if (aData[1] == clientName)
                 {
                     un.isPlayersUnit = true;
+                    go.transform.GetChild(0).gameObject.SetActive(true);
                 }
                 else
                 {
