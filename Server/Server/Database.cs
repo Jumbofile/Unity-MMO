9b4c5cde87b0c00ea6ee275d8f7456d1f5dd96cc
jumbofile <gplachno@ycp.edu>
2019-04-15 21:23:45 -0400

    Database of character select done

--- Server/Server/Database.cs
+++ Server/Server/Database.cs
@@ -36,6 +36,8 @@ namespace Server
             return pw;
         }
 
+
+
         //Checks if the user already exsists
         private static string userExists(string username)
         {
@@ -67,6 +69,44 @@ namespace Server
             return un;
         }
 
+        //Checks if the user already exsists
+        private static bool charNameTaken(string name)
+        {
+            string un = null;
+
+            try
+            {
+                SqlConnection con = new SqlConnection(@SqlSource);
+                con.Open();
+
+                SqlCommand command = new SqlCommand("SELECT Name from Characters WHERE Name = @name", con);
+                command.Parameters.AddWithValue("@name", name);
+                // int result = command.ExecuteNonQuery();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        un = String.Format("{0}", reader["Name"]);
+                    }
+                }
+
+                con.Close();
+            }
+            catch (Exception e)
+            {
+                un = null;
+            }
+            if(un != null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+
         public static bool AuthenticateUser(string username, string password)
         {
             //int result = 0;
@@ -78,14 +118,66 @@ namespace Server
             {
                 return false;
             }
-            /*result = (int)Program.form.usersTableAdapter.Authenticate(username,BCrypt.HashPassword(password,BCrypt.GenerateSalt()));
-            if (result == 1)
-            {
-                return true;
-            }
-            else return false;*/
         }
 
+        public static bool CreateCharacter(string name, int accountID, int classNum)
+        {
+            if(charNameTaken(name) == false)
+            {
+                SqlConnection con = new SqlConnection(@SqlSource);
+                con.Open();
+
+                SqlCommand command = new SqlCommand("INSERT INTO Characters(ownerAcc, name, level, class) VALUES (@id, @name, 1, @class)", con);
+                command.Parameters.AddWithValue("@name", name);
+                command.Parameters.AddWithValue("@id", accountID);
+                command.Parameters.AddWithValue("@class", classNum);
+                // int result = command.ExecuteNonQuery();
+                try
+                {
+                    command.ExecuteNonQuery();
+                    //Console.WriteLine("Succ");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return false;
+                }
+
+                con.Close();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //gets the id associated with the 
+        public static int getAccountID(string username)
+        {
+            if(userExists(username) != null)
+            {
+                SqlConnection con = new SqlConnection(SqlSource);
+                con.Open();
+
+                SqlCommand command = new SqlCommand("Select ID from Users WHERE name = @user");
+                command.Parameters.AddWithValue("@user", username);
+
+                int id;
+                try
+                {
+                    id = (Int32)command.ExecuteScalar();
+                    return id;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                    con.Close();
+                    return -1;
+                }
+            }
+            return -1;
+        }
         //Creats a user in the database
         public static bool CreateUser(string username, string password, string email)
         {
